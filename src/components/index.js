574193fbdf5d01e4dea84ca2c83b365932c58423
Aditya Kumar <adityakumar@Adityas-Air-5.lan>
2024-12-29 12:08:41 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container"
+import Logo from "./Logo"
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button"
+import RTE from "./RTE";
+import Signup from './Signup';
+import  Login  from "./Login";
+import PostForm from './post-form/PostForm'
+import AuthLayout from './AuthLayout'
+import Input from './Input'
+import PostCard from './PostCard'
+import Select from './Select'
+
+
+export {Header,Footer,Container,Logo,LogoutBtn,Button,RTE,Signup,Login,PostForm,AuthLayout,Input,PostCard,Select}
